11c31dadcfed458585817d7e92a1cabccdee8241
YHCHOI <cyh6809@gmail.com>
2023-05-09 18:50:54 +0900

    initialize java + gradle
    java oop 프로젝트 Setup

---
+++ src/main/java/com/javaoop/JavaOopApplication.java
@@ -0,0 +1,13 @@
+package com.javaoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOopApplication.class, args);
+	}
+
+}
